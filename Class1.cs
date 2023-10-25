7c898f1b5a80c4f12d7dc7706e39300e2ed09d39
captain-nemo-10994 <msa10994@tutanota.com>
2023-10-25 01:28:44 -0500

    Hello, World!

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        static void Main(string[] args)
+        {
+
+            // print statement
+            Console.WriteLine("Hello, World!");
+
+            // To prevents the screen from  
+            // running and closing quickly 
+            Console.ReadKey();
+        }
+    }
+}
