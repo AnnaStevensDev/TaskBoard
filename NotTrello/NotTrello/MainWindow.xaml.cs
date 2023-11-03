dfdf542b9364d08deec48eca920fb829239c8bd9
Anna Stevens <AnnaStevens@letu.edu>
2023-11-03 11:49:47 -0500

    Added stack panels and changed the window mode to fullscreen borderless.

--- NotTrello/NotTrello/MainWindow.xaml.cs
+++ NotTrello/NotTrello/MainWindow.xaml.cs
@@ -23,6 +23,15 @@ namespace NotTrello
         public MainWindow()
         {
             InitializeComponent();
+            this.PreviewKeyDown += new KeyEventHandler(HandleEsc);
+        }
+
+        private void HandleEsc(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                this.Close();
         }
     }
+    
+
 }
