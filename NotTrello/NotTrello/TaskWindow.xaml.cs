271c1ceb25c5e86bc1b41d51aece18321c43584f
Anna Stevens <AnnaStevens@letu.edu>
2023-11-21 16:52:09 -0600

    Added date and color picker to task window and got rid of the comments section.

--- NotTrello/NotTrello/TaskWindow.xaml.cs
+++ NotTrello/NotTrello/TaskWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Haley.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Haley.Utils;
 
 namespace NotTrello
 {
@@ -54,5 +56,6 @@ namespace NotTrello
         {
 
         }
+
     }
 }
